4980f78ab2bd05eff42d1e39446f71aa903b8f89
Ascio <ascenzialessandro93@gmail.com>
2026-06-02 22:05:56 +0200

    Tentativo fix GameRoom Syntax

--- src/rooms/GameRoom.ts
+++ src/rooms/GameRoom.ts
@@ -1,7 +1,7 @@
 import { Room, Client } from "colyseus";
 import { MyState, Player } from "./schema/MyState.js";
 
-export class GameRoom extends Room<MyState> {
+export class GameRoom extends (Room as any)<MyState> {
     onCreate() {
         this.setState(new MyState());
 

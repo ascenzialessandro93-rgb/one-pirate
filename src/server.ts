dd311257f00a8e06c678778445bb22bfc6fe3614
Ascio <ascenzialessandro93@gmail.com>
2026-06-02 21:11:15 +0200

    Fix: rimosso httpServer dal transport

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,7 @@ import { uWebSocketsTransport } from "@colyseus/uwebsockets-transport";
 import http from "http";
 
 const gameServer = new Server({
-  transport: new uWebSocketsTransport({
-    httpServer: http.createServer() // <--- Nota: 'httpServer' invece di 'server'
-  })
+  transport: new uWebSocketsTransport()
 });
 
 gameServer.listen(2567);

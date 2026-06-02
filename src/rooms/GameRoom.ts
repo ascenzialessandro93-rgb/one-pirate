4ea53c7808b16e94ebe800727445f6348d6b464c
Ascio <ascenzialessandro93@gmail.com>
2026-06-02 21:23:19 +0200

    Aggiunta GameRoom e Schema

---
+++ src/rooms/GameRoom.ts
@@ -0,0 +1,24 @@
+import { Room, Client } from "colyseus";
+import { MyState, Player } from "./schema/MyState.js";
+
+export class GameRoom extends Room<MyState> {
+    onCreate() {
+        this.setState(new MyState());
+
+        // Esempio: Quando un giocatore si muove
+        this.onMessage("move", (client, data) => {
+            const player = this.state.players.get(client.sessionId);
+            player.x = data.x;
+            player.y = data.y;
+        });
+    }
+
+    onJoin(client: Client) {
+        console.log("Giocatore entrato:", client.sessionId);
+        this.state.players.set(client.sessionId, new Player());
+    }
+
+    onLeave(client: Client) {
+        this.state.players.delete(client.sessionId);
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { Server } from "colyseus";
 import { uWebSocketsTransport } from "@colyseus/uwebsockets-transport";
 import http from "http";
+import { GameRoom } from "./rooms/GameRoom.js"; // Importa la stanza
 
 const gameServer = new Server({
   transport: new uWebSocketsTransport()

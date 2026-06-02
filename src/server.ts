ee5e6e3b50c75e6ad0ba2b1e63bbed81e04bb03f
Ascio <ascenzialessandro93@gmail.com>
2026-06-02 21:09:06 +0200

    Fix configurazione TypeScript e moduli

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,10 @@ import { uWebSocketsTransport } from "@colyseus/uwebsockets-transport";
 import http from "http";
 
 const gameServer = new Server({
-  transport: new uWebSocketsTransport({ // Anche qui cambia il nome
-    server: http.createServer()
+  transport: new uWebSocketsTransport({
+    httpServer: http.createServer() // <--- Nota: 'httpServer' invece di 'server'
   })
 });
 
-// Qui definiremo le stanze (le mappe di gioco)
 gameServer.listen(2567);
 console.log("Server OnePirate online sulla porta 2567!");
